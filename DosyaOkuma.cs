faf16898666f01f7c91128c385ade062c5f11e57
sevdaghasemzadeh <125583274+sevdaghasemzadeh@users.noreply.github.com>
2024-02-05 22:27:26 +0300

    DosyaOkuma

---
+++ DosyaOkuma.cs
@@ -0,0 +1,24 @@
+//deneme.txt diye ayrı bir dosyadan bilgiyi okuyacaktır.
+
+StreamReader sr = File.OpenText("deneme.txt");
+
+var s ="";
+
+while ((s = sr.ReadLline()) != null)
+{
+   Console.WriteLine(sr.ReadLline()); 
+}
+
+
+//veya
+
+string sonuc = File.ReadAllText("deneme.txt");
+Console.WriteLine(sonuc); 
+
+
+//okunacak sey eger dizi ise
+
+
+string sonuc = File.ReadAllLines("deneme.txt");
+Console.WriteLine(sonuc(0)); 
+Console.WriteLine(sonuc(2)); 
