192dc05bec207fe0914d20e0fb29a6627bdf68c6
Sevda <125583274+sevdaghn@users.noreply.github.com>
2025-01-11 15:01:17 +0300

    Create Students.cs

---
+++ VisualProgramming/StudentList1/Students.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student7
+{
+    public class Student
+    {
+        public String Id;
+        public String Name;
+        public String Surname;
+        public int Age;
+        public String Department;
+    }
+}
